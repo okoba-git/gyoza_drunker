e978edb1162bd17048250ae1dff1133d5a0c7579
Sawako Okoba <taakiwami@gmail.com>
2026-02-26 16:52:07 +0900

    02/26作業 shop-list.php作成

---
+++ admin/shop/shop.list.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-64UC4BEhTGwk3eGpak4nO2jqtl7liTS+juXkSJ2gPAQPmlClQO7s5UgCeR6US48g" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>店舗 - 一覧</h1>
+    <a class="btn btn-primary" href="#" role="button">Link</a>
+    <script src="https://cdn.jsdelivr.net"></script>
+</body>
+
+</html>
