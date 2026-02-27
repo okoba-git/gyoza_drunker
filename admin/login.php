19dc7da66807ee254a1875862874fcc9baa1ef74
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-02-27 12:39:23 +0900

    login画面の大枠を作成

---
+++ admin/login.php
@@ -0,0 +1,56 @@
+<?php
+// session_start();
+// require_once __DIR__ . '';
+
+// if (isset($_SESSION['id'])) {
+//     header('location:index.php');
+//     exit();
+// }
+
+?>
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ログイン｜ふくおか餃子FES</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <main class="container">
+        <div>
+            <h1 class="my-5 text-center">管理画面 - ログイン</h1>
+
+            <form action="login-do.php" method="post">
+                <div class="row justify-content-center">
+                    <div class="mb-3 col-6">
+                        <label for="name" class="form-label">ID</label>
+                        <input type="text" name="userid" id="userid" class="form-control" placeholder="user ID">
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="mb-3 col-6">
+                        <label for="password" class="form-label">PASS</label>
+                        <input type="password" name="password" id="password" class="form-control" placeholder="password">
+                    </div>
+                </div>
+
+                <div class="mb-3 text-center">
+                    <input type="submit" value="ログイン" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-jdSIJTK9l6XwXj3RixpVDXtMcA2bFd9O81RlLAwhpr2oXRqvQP88rr16IeFXTgFE" crossorigin="anonymous"></script>
+</body>
+
+</html>
