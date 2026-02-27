c36d51da83b157aee81bd8a615215d858035ccf0
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-02-27 14:44:17 +0900

    login.phpのUI作成

--- admin/login.php
+++ admin/login.php
@@ -25,27 +25,28 @@
 
 <body>
     <main class="container">
-        <div>
+        <div class="l-wrapper">
             <h1 class="my-5 text-center">管理画面 - ログイン</h1>
 
             <form action="login-do.php" method="post">
-                <div class="row justify-content-center">
-                    <div class="mb-3 col-6">
-                        <label for="name" class="form-label">ID</label>
-                        <input type="text" name="userid" id="userid" class="form-control" placeholder="user ID">
+                <div class="row justify-content-center align-items-center mb-4">
+                    <label for="id" class="col-sm-1 col-form-label col-form-label-lg">ID</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" id="userid" placeholder="user ID">
                     </div>
                 </div>
 
-                <div class="row justify-content-center">
-                    <div class="mb-3 col-6">
-                        <label for="password" class="form-label">PASS</label>
+                <div class="row justify-content-center align-items-center mb-5">
+                    <label for="password" class="col-sm-1 col-form-label col-form-label-lg">PASS</label>
+                    <div class="col-sm-5">
                         <input type="password" name="password" id="password" class="form-control" placeholder="password">
                     </div>
                 </div>
 
                 <div class="mb-3 text-center">
-                    <input type="submit" value="ログイン" class="btn btn-primary">
+                    <input type="submit" value="ログイン" class="btn btn-primary btn-lg">
                 </div>
+
             </form>
         </div>
     </main>
