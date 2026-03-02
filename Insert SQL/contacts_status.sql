16b20f774718661a9269580e12c109f6f3827a03
Yoshiaki Koike <koike.y44@gmail.com>
2026-03-02 14:35:16 +0900

    doc: お問い合わせテーブルからいらないカラムを削除するSQLを追加

--- Insert SQL/contacts_status.sql
+++ Insert SQL/contacts_status.sql
@@ -3,4 +3,7 @@ INSERT INTO `contact_status`(`name`, `create_at`, `update_at`) VALUES
 ('未対応',now(),now()),
 ('対応中',now(),now()),
 ('完了',now(),now()),
-('対応不要',now(),now())
+('対応不要',now(),now());
+
+-- 不必要なカラムの削除
+ALTER TABLE `contacts` DROP `date`;
