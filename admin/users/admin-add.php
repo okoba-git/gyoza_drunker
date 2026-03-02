a105b8928b38444a4f32506209c04e1433bcb971
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-03-02 12:53:06 +0900

    ユーザー追加処理機能を作成

--- admin/users/admin-add.php
+++ admin/users/admin-add.php
@@ -1,26 +1,34 @@
 <?php
 require_once __DIR__ . ('/../../inc/config.php');
 require_once __DIR__ . ('/../../inc/function.php');
+
+session_start();
 ?>
 <!doctype html>
 <html lang="ja">
 
 <head>
     <title>管理者登録｜ふくおか餃子FES</title>
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
     <?php
-    require_once __DIR__ . ('/../inc/header.php');
+    require_once __DIR__ .  '/../inc/header.php';
     ?>
 
     <main role="main" class="container" style="padding:60px 15px 0">
+        <?php if (!empty($_SESSION['success'])): ?>
+            <div class="alert alert-success">登録完了しました！</div>
+            <?php unset($_SESSION['success']); ?>
+        <?php endif; ?>
+
         <div>
             <h1 class="my-5">管理者ユーザー - 追加</h1>
 
-            <form action="user_add_do.php" method="post">
+            <form action="admin-add-do.php" method="post">
                 <div class="mb-3 col">
                     <label for="name" class="form-label">ユーザー名</label>
                     <input type="text" name="name" id="name" class="form-control">
@@ -32,16 +40,13 @@ require_once __DIR__ . ('/../../inc/function.php');
                 </div>
 
                 <div class="d-flex flex-row gap-2">
-                    <div>
-                        <input type="submit" value="追加" class="btn btn-primary">
-                    </div>
-                    <div>
-                        <input type="submit" value="キャンセル" class="btn btn-primary">
-                    </div>
+                    <input type="submit" value="追加" class="btn btn-primary btn-lg" style="min-width:120px;">
+                    <a class="btn btn-primary btn-lg" href="admin-add.php" style="min-width:120px;">キャンセル</a>
                 </div>
             </form>
 
         </div>
+
     </main>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
