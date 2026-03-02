caaa26114692a7eb46a8d290e4d67d88b47e04bb
Yoshiaki Koike <koike.y44@gmail.com>
2026-03-02 16:13:23 +0900

    feature: お問い合わせ一覧ページの作成

---
+++ admin/contact/contact-list.php
@@ -0,0 +1,83 @@
+<?php
+require_once __DIR__ . ('/../../inc/function.php');
+
+$contacts = [];
+$status = [];
+try {
+  $db = db_connect();
+  $sql = 'SELECT * FROM contacts';
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  $sql = 'SELECT * FROM contact_status';
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $status = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+  debug_log($e->getMessage());
+}
+?>
+
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>お問い合わせ一覧 | 餃子FES管理</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+    rel="stylesheet">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <?php require_once __DIR__ . '/../inc/header.php'; ?>
+  <main class="container">
+    <div class="l-wrapper">
+      <h1 class="my-5 text-center">お問い合わせ - 一覧</h1>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th class="col">id</th>
+            <th class="col">name</th>
+            <th class="col">email</th>
+            <th class="col">tel</th>
+            <th class="col">content</th>
+            <th class="col">state</th>
+            <th class="col">create_at</th>
+            <th class="col">update_at</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($contacts as $contact): ?>
+            <tr>
+              <td class="col"><?php echo $contact['id']; ?></td>
+              <td class="col-1"><?php echo $contact['name']; ?></td>
+              <td class="col-2"><?php echo $contact['email']; ?></td>
+              <td class="col-2"><?php echo $contact['tel']; ?></td>
+              <td class="col-3"><?php echo nl2br($contact['body']); ?></td>
+              <td class="col-2">
+                <select name="state" class="form-select" aria-label="Default select example" id="contact_state">
+                  <?php foreach ($status as $state): ?>
+                    <?php $selected = $content['state'] === $state['id'] ? 'selected' : '' ; ?>
+                    <option value="<?php echo $state['id']; ?>" <?php echo $selected; ?>><?php echo $state['name']; ?></option>
+                  <?php endforeach; ?>
+                </select>
+              </td>
+              <td class="col"><?php echo $contact['create_at']; ?></td>
+              <td class="col"><?php echo $contact['update_at']; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+  </main>
+
+  <!-- Bootstrap Javascript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-jdSIJTK9l6XwXj3RixpVDXtMcA2bFd9O81RlLAwhpr2oXRqvQP88rr16IeFXTgFE" crossorigin="anonymous"></script>
+</body>
+
+</html>
