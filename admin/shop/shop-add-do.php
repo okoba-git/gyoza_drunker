361f4d844fc47eb5bdb594c1d485b5cc08f3da9d
Sawako Okoba <taakiwami@gmail.com>
2026-03-02 15:10:14 +0900

    店舗追加機能実装

---
+++ admin/shop/shop-add-do.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/../../inc/function.php';
+require_once __DIR__ . '/../../inc/config.php';
+
+// TODO: データ受け取り
+if (!empty($_POST)) {
+    // POST送信されたとき
+    if (!empty($_POST['shop-name']) && !empty($_POST['shop_num']) && !empty($_POST['body'])) {
+        // TODO: 必須項目チェック（空の場合）
+        $name = $_POST['shop-name'];
+        $shop_num = $_POST['shop_num'];
+        $body = $_POST['body'];
+        // 日付けが空文字だったら当日のデータ、空文字じゃなかったら送信されたデータを代入
+        $date = empty($_POST['date']) ? date('Y-m-d') : $_POST['date'];
+
+        // DBに接続
+        try {
+            $db = db_connect();
+            // shopsに1行挿入するSQL
+            $sql = 'INSERT INTO shops (shop_num,name,body,create_at) VALUES (:shop_num,:name,:body,:create_at)';
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':shop_num', $shop_num, PDO::PARAM_STR);
+            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+            $stmt->bindParam(':body', $body, PDO::PARAM_STR);
+            $stmt->bindParam(':create_at', $date, PDO::PARAM_STR);
+            $stmt->execute();
+
+            // トップページへ画面遷移
+            header('location:shop-list.php');
+            exit();
+        } catch (PDOException $e) {
+            exit('エラー: ' . $e->getMessage());
+        }
+    }
+}
---
+++ admin/shop/shop-add.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>店舗 - 詳細</title>
+</head>
+
+<body class="l-wrapper">
+    <?php require_once __DIR__ . ('/../inc/header.php'); ?>
+    <h1 class="mb-5">店舗 - 追加</h1>
+
+    <form action="shop-add-do.php" method="post" class="needs-validation mb-3" novalidate>
+        <div class="mb-3">
+            <label for="name" class="form-label">店舗名</label>
+            <input type="text" name="shop-name" id="shop-name" class="form-control" required>
+            <div class="invalid-feedback">
+                店舗名を入力してください
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <div class="col">
+                <label for="date" class="form-label">追加日</label>
+                <input type="date" name="date" id="date" class="form-control">
+            </div>
+            <div class="col">
+                <label for="shop_num" class="form-label">ブース番号</label>
+                <input type="text" name="shop_num" id="shop_num" class="form-control" required>
+                <div class="invalid-feedback">
+                    ブース番号を入力してください
+                </div>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="body" class="form-label">説明文</label>
+            <textarea name="body" id="body" class="form-control" required></textarea>
+            <div class="invalid-feedback">
+                店舗の説明文を入力してください
+            </div>
+        </div>
+        <div class="mb-3">
+            <input type="submit" value="完了" class="btn btn-primary">
+            <a href="shop-list.php" class="btn btn-secondary">キャンセル</a>
+        </div>
+    </form>
+
+
+    <div class=" text-center">
+        <a href="shop-list.php" class="btn btn-primary mt-3">店舗一覧に戻る</a>
+    </div>
+
+
+    <!-- Bootstrap Javascript(jQuery含む) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>
+        window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
