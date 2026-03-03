21a9fbc4dcdb14e619743baeb5456c1f8b000495
Yoshiaki Koike <koike.y44@gmail.com>
2026-03-03 12:26:18 +0900

    feature: bootstrapの参照の修正、ステータス変更処理の為のクラス属性とid属性を修正

--- admin/contact/contact-list.php
+++ admin/contact/contact-list.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . ('/../../inc/function.php');
+require_once __DIR__ . '/../../inc/function.php';
 
 // お問い合わせとお問い合わせステータス用配列
 $contacts = [];
@@ -12,7 +12,7 @@ try {
   $stmt = $db->prepare($sql);
   $stmt->execute();
   $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  
+
   //お問い合わせ状態テーブルからすべてのステータスを取得 
   $sql = 'SELECT * FROM contact_status';
   $stmt = $db->prepare($sql);
@@ -32,9 +32,7 @@ try {
 
   <title>お問い合わせ一覧 | 餃子FES管理</title>
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
-    rel="stylesheet">
-  <link rel="stylesheet" href="./css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -42,6 +40,7 @@ try {
   <main class="container">
     <div class="l-wrapper">
       <h1 class="my-5 text-center">お問い合わせ - 一覧</h1>
+      <div id="message-area"></div>
       <table class="table table-bordered">
         <thead>
           <tr>
@@ -49,16 +48,16 @@ try {
             <th class="col">name</th>
             <th class="col">email</th>
             <th class="col">tel</th>
-            <th class="col">content</th>
+            <th class="col">body</th>
             <th class="col">state</th>
             <th class="col">create_at</th>
             <th class="col">update_at</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody id="contact_tbody">
           <!-- 一覧をテーブルで表示 -->
           <?php foreach ($contacts as $contact): ?>
-            <tr>
+            <tr data-contact-id="<?php echo $contact['id'] ?>">
               <td class="col"><?php echo $contact['id']; ?></td>
               <td class="col-1"><?php echo $contact['name']; ?></td>
               <td class="col-2"><?php echo $contact['email']; ?></td>
@@ -66,15 +65,15 @@ try {
               <td class="col-3"><?php echo nl2br($contact['body']); ?></td>
               <td class="col-2">
                 <!-- 状態はセレクトボックスで表示 -->
-                <select name="state" class="form-select" aria-label="Default select example" id="contact_state">
+                <select name="state" class="form-select" aria-label="Default select example">
                   <?php foreach ($status as $state): ?>
-                    <?php $selected = $content['state'] === $state['id'] ? 'selected' : '' ; ?>
+                    <?php $selected = $contact['state'] === $state['id'] ? 'selected' : ''; ?>
                     <option value="<?php echo $state['id']; ?>" <?php echo $selected; ?>><?php echo $state['name']; ?></option>
                   <?php endforeach; ?>
                 </select>
               </td>
               <td class="col"><?php echo $contact['create_at']; ?></td>
-              <td class="col" id="update_at"><?php echo $contact['update_at']; ?></td>
+              <td class="col update-at-column"><?php echo $contact['update_at']; ?></td>
             </tr>
           <?php endforeach; ?>
         </tbody>
@@ -83,7 +82,9 @@ try {
   </main>
 
   <!-- Bootstrap Javascript -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-jdSIJTK9l6XwXj3RixpVDXtMcA2bFd9O81RlLAwhpr2oXRqvQP88rr16IeFXTgFE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"></script>
+  <script></script>
+  <script src="../../js/change-contact-state.js" type="module"></script>
 </body>
 
 </html>
