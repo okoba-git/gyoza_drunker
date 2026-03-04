70e720bb09cc8cf394588f9f1bc1db494af45866
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-03-04 11:49:54 +0900

    list→detail.phpへ接続するためのaタグを追記。

--- admin/news/news-list.php
+++ admin/news/news-list.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . ('/../../inc/config.php');
 require_once __DIR__ . ('/../../inc/function.php');
+
 // DBに接続
 $db = db_connect();
-$sql = 'SELECT title,create_at FROM news ORDER BY create_at DESC';
+$sql = 'SELECT * FROM news ORDER BY create_at DESC';
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -16,45 +17,44 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS -->
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/style.css">
-    <title>おしらせ - 一覧</title>
+    <title>お知らせ - 一覧</title>
 </head>
-<body class="l-wrapper">
-    <h1 class="c-title">お知らせ- 一覧</h1>
 
-        <!-- <div class="c-section"> 
-            <div class="c-news-content c-news__other">
-                <dl class="c-news-detail">
-                    <dt class="c-news-date">
-                        <time datetime="2030-02-25">2030.2.25</time>（月）
-                    </dt>
-                    <dd class="c-news-title">
-                        <a href="#">出店者インタビュー　博多区で人気の「博多ぎょうざ堂」</a>
-                    </dd>
-                </dl>
-            </div>
-        </div> -->
-   
-    <a class="btn btn-primary mb-3" href="news-add.php">新規作成</a>
+<body class="l-wrapper">
+
+    <?php
+    require_once __DIR__ .  '/../inc/header.php';
+    ?>
+
+    <h1 class="c-title">お知らせ - 一覧</h1>
+
+    <a class="btn btn-primary mb-3" href="./news-add.php">新規作成</a>
     <table class="table table-bordered">
         <thead>
-            <tr class="">
+            <tr>
                 <th class="col-1">作成日</th>
-                <th class="col-2">タイトル</th>
+                <th class="col-1">作成者</th>
+                <th class="col-4">タイトル</th>
             </tr>
         </thead>
         <?php foreach ($result as $news): ?>
             <tbody>
-                <tr class="">
-                    <td class="col-1"><?php echo $news['create_at']; ?></td>
-                    <td class="col-2"><?php echo $news['title']; ?></td>
+                <tr>
+                    <td><?php echo $news['create_at']; ?></td>
+                    <td><?php echo $news['author']; ?></td>
+                    <td><a href="news-detail.php?id=<?php echo $news['id']; ?>"><?php echo $news['title']; ?></a></td>
                     </td>
                 </tr>
             </tbody>
         <?php endforeach; ?>
-
     </table>
+
+    <div class=" text-center">
+        <a href="../index.php" class="btn btn-primary mt-3">TOPに戻る</a>
+    </div>
+
     <!-- Bootstrap Javascript(jQuery含む) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
     <script>
