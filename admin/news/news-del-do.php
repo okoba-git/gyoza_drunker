1ad074214645a685073d692aa754ed1c845f6e98
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-03-04 14:42:28 +0900

    news-detailに削除機能を反映。

---
+++ admin/news/news-del-do.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . ('/../../inc/config.php');
+require_once __DIR__ . ('/../../inc/function.php');
+
+session_start();
+
+// データ受け取り
+if (!empty($_POST)) {
+    // POST送信されたとき
+    if (!empty($_POST['id'])) {
+        // idのチェック（空の場合）
+        $id = $_POST['id'];
+        // DBに接続
+        try {
+            $db = db_connect();
+            // usersテーブルから1行削除するSQL
+            $sql = 'DELETE FROM news WHERE id=:id';
+            $stmt = $db->prepare($sql);
+            // idをプレースホルダへバインド
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $_SESSION['success'] = '削除しました。';
+
+            // トップページへ画面遷移
+            header('location:news-list.php');
+            exit();
+        } catch (PDOException $e) {
+            exit('エラー: ' . $e->getMessage());
+        }
+    }
+}
